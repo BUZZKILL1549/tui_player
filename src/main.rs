e27cb7c55ea579cdcb2171c11d5bb740da5ecb73
BUZZKILL1549 <aushvs10@hotmail.com>
2025-03-04 21:16:00 +0530

    Basic Page V2

--- src/main.rs
+++ src/main.rs
@@ -16,16 +16,16 @@ fn main() {
     ratatui::restore();
 }
 
+
 fn draw(frame: &mut Frame) {
     use Constraint::{Fill, Length, Min};
 
-    let vertical = Layout::vertical([Length(1), Min(0), Length(1)]);
-    let [title_area, main_area, status_area] = vertical.areas(frame.area());
+    let vertical = Layout::vertical([Length(1), Min(0)]);
+    let [title_area, main_area] = vertical.areas(frame.area());
     let horizontal = Layout::horizontal([Fill(1); 2]);
     let [left_area, right_area] = horizontal.areas(main_area);
 
-    frame.render_widget(Block::bordered().title("Title Bar"), title_area);
-    frame.render_widget(Block::bordered().title("Status Bar"), status_area);
-    frame.render_widget(Block::bordered().title("Left"), left_area);
-    frame.render_widget(Block::bordered().title("Right"), right_area);
+    frame.render_widget(Block::bordered().title("Music Player"), title_area);
+    frame.render_widget(Block::bordered().title("Music List"), left_area);
+    frame.render_widget(Block::bordered().title("Music Info"), right_area);
 }
