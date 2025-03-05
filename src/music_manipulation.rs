ec43e363f98baa7e7be8cfaf0ae1eff9cdf33547
BUZZKILL1549 <aushvs10@hotmail.com>
2025-03-06 00:05:22 +0530

    Music Info V2

--- src/music_manipulation.rs
+++ src/music_manipulation.rs
@@ -1,5 +1,9 @@
 use std::path::PathBuf;
 use walkdir::WalkDir;
+use lofty::{
+    prelude::*,
+    probe::Probe
+};
 
 pub fn get_music(directory: &str) -> Vec<PathBuf> {
     let music_extensions = [
@@ -35,3 +39,34 @@ pub fn convert_to_string(paths: &Vec<PathBuf>) -> Vec<String> {
         .collect()
 }
 
+pub fn get_music_tags(path: &str) -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
+    let tagged_file = Probe::open(path)
+        .expect("ERROR: Bad path")
+        .read()
+        .expect("ERROR: Failed to read file");
+
+    let tag = match tagged_file.primary_tag() {
+        Some(primary_tag) => primary_tag,
+        None => tagged_file.first_tag().expect("ERROR: No tags found"),
+    };
+
+    let mut tags = Vec::new();
+
+    if let Some(title) = tag.title() {
+        tags.push(("Title".to_string(), title.to_string()));
+    }
+
+    if let Some(artist) = tag.artist() {
+        tags.push(("Artist".to_string(), artist.to_string()));
+    } 
+
+    if let Some(album) = tag.album() {
+        tags.push(("Album".to_string(), album.to_string()));
+    }
+
+    if let Some(genre) = tag.genre() {
+        tags.push(("Genre".to_string(), genre.to_string()));
+    }
+
+    Ok(tags)
+} 
